7393a8fd54f88e037ba4ca0573121b505fce812c
e5LA <208197507+e5LA@users.noreply.github.com>
2025-06-23 14:17:41 +0200

    refactor: cosmetic, formatting

--- secure-rest-api/secure-rest-api-server/src/test/java/com/realworldjava/securerestapi/server/converters/KeycloakJwtAuthenticationConverterTest.java
+++ secure-rest-api/secure-rest-api-server/src/test/java/com/realworldjava/securerestapi/server/converters/KeycloakJwtAuthenticationConverterTest.java
@@ -21,13 +21,16 @@ class KeycloakJwtAuthenticationConverterTest {
 
   @Test
   void convert_withValidRealmRoles_returnsCorrectAuthorities() {
+    // given
     Jwt jwt = Jwt.withTokenValue("token")
         .header("alg", "none")
         .claim("realm_access", Map.of("roles", List.of("ADMIN", "READER")))
         .build();
 
+    // when
     JwtAuthenticationToken token = (JwtAuthenticationToken) converter.convert(jwt);
 
+    // then
     assertEquals(2, token.getAuthorities().size());
     assertTrue(token.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN")));
     assertTrue(token.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_READER")));
@@ -35,13 +38,16 @@ class KeycloakJwtAuthenticationConverterTest {
 
   @Test
   void convert_withNoRealmAccess_returnsEmptyAuthorities() {
+    // given
     Jwt jwt = Jwt.withTokenValue("token")
         .header("alg", "none")
         .claim("clm", "vl")
         .build();
 
+    // when
     JwtAuthenticationToken token = (JwtAuthenticationToken) converter.convert(jwt);
 
+    // then
     assertTrue(token.getAuthorities().isEmpty());
   }
 
